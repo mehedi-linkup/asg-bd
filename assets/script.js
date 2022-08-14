9ee26b4d5acfdebb2361f92c1f51fbe05d73b4e6
mehedi-linkup <mehedi.linkup@gmail.com>
2022-08-14 17:20:29 +0600

    Latest Change

---
+++ assets/script.js
@@ -0,0 +1,93 @@
+// Who are we section carousel
+$('.message .owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    dots:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:2
+        }
+    }
+});
+$('.first-carousel.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    dots:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+});
+$('.second-carousel.owl-carousel').owlCarousel({
+    margin:10,
+    nav:false,
+    dots:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:4
+        }
+    }
+});
+$('.third-carousel.owl-carousel').owlCarousel({
+    margin:10,
+    loop:true,
+    nav:false,
+    dots:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:4
+        }
+    }
+});
+
+
+// Carousel Wall Section
+$('.wall .owl-carousel').owlCarousel({
+    margin:10,
+    loop:true,
+    nav:false,
+    dots:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:4
+        }
+    }
+});
+
+// Image View Box - Business Wall Section
+$(function(){
+	$('.image-link').viewbox();
+});
